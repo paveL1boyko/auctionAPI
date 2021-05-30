2c9e295d629763eecc1f5c03e24a68d40ba6761d
Pavel Boyko <pave1.boyk0ok@gmail.com>
2021-05-30 18:51:46 +0300

    test acctions

--- app/authenticate/urls.py
+++ app/authenticate/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('registration/', RegistrationAPIView.as_view(), name='registration'),
     path('token/', LoginAPIView.as_view(), name='token'),
     path('user/', UserRetrieveAPIView.as_view(), name='user_data')
+
 ]
